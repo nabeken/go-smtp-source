2c1f327036716bc20e352018951ada7a6f93dc2e
TANABE Ken-ichi <nabeken@tknetworks.org>
2023-06-25 15:52:21 +0900

    run the bench with multiple recepients

--- bench_check.sh
+++ bench_check.sh
@@ -28,25 +28,27 @@ echo "Start sending $M messages... (GOMAXPROCS=${GOMAXPROCS:-default})"
 echo
 
 for s in 1 100 1000; do
-  echo "Concurrency: $s"
-  echo "smtp-source:"
-  /usr/bin/time smtp-source -s $s -m $M -f from@example.com -t to@example.com -M smtp.example.com ${HOST}:${PORT}
-  check && reset_dump
+  for r in 1 3; do
+    echo "Concurrency: $s / Recipients: $r"
+    echo "smtp-source:"
+    /usr/bin/time smtp-source -s $s -m $M -r $r -f from@example.com -t to@example.com -M smtp.example.com ${HOST}:${PORT}
+    check && reset_dump
 
-  echo
-  echo "go-smtp-source:"
-  /usr/bin/time go-smtp-source -s $s -m $M -resolve-once ${HOST}:${PORT}
-  check && reset_dump
+    echo
+    echo "go-smtp-source:"
+    /usr/bin/time go-smtp-source -s $s -m $M -r $r -resolve-once ${HOST}:${PORT}
+    check && reset_dump
 
-  echo
-  echo "smtp-source (-d):"
-  /usr/bin/time smtp-source -d -s $s -m $M -f from@example.com -t to@example.com -M smtp.example.com ${HOST}:${PORT}
-  check && reset_dump
+    echo
+    echo "smtp-source (-d):"
+    /usr/bin/time smtp-source -d -s $s -m $M -r $r -f from@example.com -t to@example.com -M smtp.example.com ${HOST}:${PORT}
+    check && reset_dump
 
-  echo
-  echo "go-smtp-source (-d):"
-  /usr/bin/time go-smtp-source -d -s $s -m $M -resolve-once ${HOST}:${PORT}
-  check && reset_dump
+    echo
+    echo "go-smtp-source (-d):"
+    /usr/bin/time go-smtp-source -d -s $s -m $M -r $r -resolve-once ${HOST}:${PORT}
+    check && reset_dump
 
-  echo "-------------------------"
+    echo "-------------------------"
+  done
 done
